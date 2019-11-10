8a80718e678339f3f7dbadcf559404dd30f2f34e
Avery Wiens <aewiens@uga.edu>
2019-11-09 21:26:16 -0500

    barest bones ridge regression

--- machine-learning/regression/module/ridge.py
+++ machine-learning/regression/module/ridge.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 
 
-class OLS:
+class RidgeRegression:
     """ 
-    Ordinary Least Squares Linear Regression
+    Ridge Regression
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, alpha=1.0):
+
+        self.alpha = alpha
 
 
     def augment(self, x):
@@ -20,8 +21,9 @@ class OLS:
     def fit(self, x, y):
 
         X = self.augment(x)
+        aI = np.diag([self.alpha] * len(X.T))
 
-        self.beta = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.beta = np.linalg.inv(X.T @ X + aI) @ X.T @ y
 
         return self
 
@@ -37,8 +39,6 @@ class OLS:
 
         n = len(x)
 
-        #x, y = np.ravel(x), np.ravel(y)
-
         x_, y_ = x.mean(), y.mean()
 
         SS = np.var(x) ** 0.5 * np.var(y) ** 0.5
@@ -48,7 +48,6 @@ class OLS:
         return R**2
         
 
-
 if __name__ == '__main__':
    
     # Input column vectors to OLS class
@@ -56,18 +55,20 @@ if __name__ == '__main__':
     y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12]).reshape(-1, 1)
 
     # Train a linear regression with my OLS class
-    myLR = OLS().fit(x, y)
+    myRidge = RidgeRegression(alpha=0.1).fit(x, y)
 
     # Predict at inputs
-    yhat = myLR.predict(x)
+    yhat = myRidge.predict(x)
 
     # Test against scikit-learn
-    lr = LinearRegression().fit(x, y)
-    assert np.allclose(yhat, lr.predict(x))
+    ridge = Ridge(alpha=0.1).fit(x, y)
+    #assert np.allclose(yhat, ridge.predict(x))
+    print(yhat)
+    print(ridge.predict(x))
 
     # Score
-    score = myLR.score(x, y)
-    print("OLS R^2 value: {:>2.3f}".format(score))
+    #score = myRR.score(x, y)
+    #print("OLS R^2 value: {:>2.3f}".format(score))
 
     # Plot observations and LR prediction
     grid = np.linspace(0, 10)
